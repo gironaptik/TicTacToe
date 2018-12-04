5dd162fa3f1de8a2de7222b64e970b98731fe229
Giron Aptik <giron.aptik@gmail.com>
2018-12-04 08:08:23 +0200

    Player update

---
+++ src/Player.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+
+public class Player {
+	
+	private String name;
+	private Map<Integer, String> userTurn;
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Map<Integer, String> getUserTurn() {
+		return userTurn;
+	}
+	public void setUserTurn(Map<Integer, String> userTurn) {
+		this.userTurn = userTurn;
+	}
+	
+
+
+	
+}
