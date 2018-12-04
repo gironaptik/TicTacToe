a8aa732c2e732c479aef7a6a61bd1a1f04bc9c63
Shai <Shai@DESKTOP-4V64QLS>
2018-12-04 09:02:42 +0200

    board update

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,33 @@
 
 public class Board {
 	
+	
+	private int currentRow, currentCol
 	private String[][] Position = new String[3][3];
 
+	public Board(String[][] position) {
+		super();
+		Position = position;
+	}
+
+	public String[][] getPosition() {
+		return Position;
+	}
+
+	public void setPosition(String[][] position) {
+		Position = position;
+	}
+
+	public void checkAndPlayMove(int choice){
+		
+		
+		Convert(choice);
+		
+	}
+	
+	public void checkResult(){
+		
+	}
+	
 
 }
